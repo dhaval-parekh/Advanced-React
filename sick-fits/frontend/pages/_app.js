9f46d5dd1ff453438bb3d17a438b5095f7974afd
Dhaval Parekh <dmparekh007@gmail.com>
2021-05-16 17:46:48 +0530

    V-9 Add styled components flicker

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,4 +1,14 @@
 import Page from '../components/Page';
+import Router  from 'next/router';
+
+// Todo: swap with our own.
+import NProgress from 'nprogress';
+// import 'nprogress/nprogress.css';
+import '../components/styles/nprogress.css'
+
+Router.events.on( 'routeChangeStart', () => NProgress.start() );
+Router.events.on( 'routeChangeComplete', () => NProgress.done() );
+Router.events.on( 'routeChangeError', () => NProgress.done() );
 
 export default function App( { Component, pageProps } ) {
 	return (
