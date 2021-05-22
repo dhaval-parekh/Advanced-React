183c196effe3c0b8a9f10c7f16622bed015711ef
Dhaval Parekh <dmparekh007@gmail.com>
2021-05-22 17:15:39 +0530

    V-14 Added auth

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -1,14 +1,27 @@
 import 'dotenv/config';
 import {config, createSchema} from "@keystone-next/keystone/schema";
 import {User} from "./schemas/User";
+import {createAuth} from "@keystone-next/auth";
+import {statelessSessions, withItemData} from "@keystone-next/keystone/session";
 
 const databaseURL = process.env.DATABASE_URL || 'mongodb://localhost/keystone-database';
+
 const sessionConfig = {
-	maxAge: (60 * 60 * 24 * 30), // How long user user should sign in.
+	maxAge: (60 * 60 * 24 * 30), // How long user should sign in.
 	secret: process.env.COOKIE_SECRET,
 };
 
-export default config({
+const {withAuth} = createAuth({
+	listKey: 'User',
+	identityField: 'email',
+	secretField: 'password',
+	initFirstItem: {
+		fields: ['name', 'email', 'password'],
+		// @Todo: add initial role.
+	}
+});
+
+export default withAuth(config({
 	server: {
 		cors: {
 			origin: [process.env.FRONTEND_URL],
@@ -24,10 +37,12 @@ export default config({
 		User
 	}),
 	ui: {
-		// Change this for roles,
-		isAccessAllowed: () => {
-			return true;
+		// Show UI for only use that pass this.
+		isAccessAllowed: ({session}) => {
+			return !! session ? session.data : false;
 		}
 	},
-	// @Todo: Session values.
-})
+	session: withItemData(statelessSessions(sessionConfig), {
+		User: `id name email`,
+	})
+}));
